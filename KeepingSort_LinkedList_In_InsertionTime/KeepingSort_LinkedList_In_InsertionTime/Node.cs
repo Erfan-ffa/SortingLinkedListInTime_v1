93b0bec57675e4e43a9f6410768fbe0a04cfbcca
Erfan Faridi <109587086+Erfan-ffa@users.noreply.github.com>
2022-08-20 11:39:34 +0430

    Add files via upload

---
+++ KeepingSort_LinkedList_In_InsertionTime/KeepingSort_LinkedList_In_InsertionTime/Node.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeepingSort_LinkedList_In_InsertionTime
+{
+    public class Node
+    {
+        public int value;
+        public Node next;
+        public Node prev;
+    }
+
+    public class NodeComparer
+    {
+        Node head = null;
+
+        public bool IsNull()
+        {
+            if (head == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public void Insert(int value)
+        {
+            if (IsNull())
+            {
+                Node newnode = new Node();
+                newnode.value = value;
+                head = newnode;
+            }
+            else
+            {
+                Node newnode = new Node();
+                newnode.value = value;
+
+                Node tempHead = head;
+
+                if (tempHead.next == null)
+                {
+                    if (tempHead.value > newnode.value)
+                    {
+                        tempHead.prev = newnode;
+                        newnode.next = tempHead;
+                        head = newnode;
+                        return;
+                    }
+                    else
+                    {
+                        tempHead.next = newnode;
+                        newnode.prev = tempHead;
+                    }
+                }
+
+                while (tempHead.next != null)
+                {
+                    if (tempHead.value > newnode.value)
+                    {
+                        if (tempHead.prev == null)
+                        {
+                            tempHead.prev = newnode;
+                            newnode.next = tempHead;
+                            head = newnode;
+                            return;
+                        }
+                        else
+                        {
+                            newnode.prev = tempHead.prev;
+                            tempHead.prev.next = newnode;
+                            newnode.next = tempHead;
+                            tempHead.prev = newnode;
+                            return;
+                        }
+                    }
+                    tempHead = tempHead.next;
+                }
+
+                if (tempHead.value > newnode.value)
+                {
+                    newnode.prev = tempHead.prev;
+                    tempHead.prev = newnode;
+                    newnode.next = tempHead;
+                    tempHead.next = null;
+                }
+                else
+                {
+                    tempHead.next = newnode;
+                    newnode.prev = tempHead;
+                }
+
+            }
+        }
+
+        public void Print()
+        {
+            if (!IsNull())
+            {
+                var te = head;
+                while (te != null)
+                {
+                    Console.WriteLine(te.value);
+                    te = te.next;
+                }
+            }
+        }
+    }
+
+}
---
+++ KeepingSort_LinkedList_In_InsertionTime/KeepingSort_LinkedList_In_InsertionTime/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using KeepingSort_LinkedList_In_InsertionTime;
+
+NodeComparer x = new NodeComparer();
+x.Insert(6);
+x.Insert(2);
+x.Insert(20);
+x.Insert(3);
+x.Insert(1);
+x.Insert(4);
+x.Print();
